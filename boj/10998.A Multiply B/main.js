e6ea6e851ab58d1ee83361c9dfdca4f2e4264e18
tmxhsk99 <tmxhsk99@naver.com>
2023-03-12 16:40:47 +0900

    feat : 백준 문제 입출력 문제 추가 및 값 입력 샘플 추가

---
+++ boj/10998.A Multiply B/main.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+let input = fs.readFileSync(filePath).toString().split("\n");
+//console.log(input);
+input = input[0];
+//console.log(input);
+input = input.split(" ").map((item) => +item);
+//console.log(input);
+
+solution(input[0], input[1]);
+
+function solution(A, B) {
+    console.log(A * B);
+}
