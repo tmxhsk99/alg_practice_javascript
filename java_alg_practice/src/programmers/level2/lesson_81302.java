8dd505cdde708d30ef4284d38278684c83ef4706
tmxhsk99 <tmxhsk99@naver.com>
2023-04-27 16:58:45 +0900

    feat : 거리두기 문제 풀이 추가

--- java_alg_practice/src/programmers/level2/lesson_81302.java
+++ java_alg_practice/src/programmers/level2/lesson_81302.java
@@ -1,6 +1,5 @@
 package programmers.level2;
 
-import java.util.Arrays;
 
 /**
  * 거리 두기 확인하기 Level2
@@ -16,101 +15,167 @@ public class lesson_81302 {
         };
         Solution solution = new Solution();
         int[] solution1 = solution.solution(palces);
-
+        for (int i : solution1) {
+            System.out.println(solution1[i]);
+        }
 
     }
 
     private static class Solution {
+        private static final int dx[] = {0, -1, 1, 0}; // 상, 좌 , 우, 하
+        private static final int dy[] = {-1, 0, 0, 1};
 
-        int[] solution(String[][] places) {
-            int[] answer = new int[5];
-            //1. places 에서 하나의 대기실을 꺼낸다
-            Arrays.stream(places)
-                    .forEach(s -> {
-                        String[] place = (String[]) s;
+        public int[] solution(String[][] places) {
 
-                        // 대기실 별로 char배열로 만든다.
-                        char[][] waitRooms = placeToWaitRooms(place);
-
-                        printWaitRooms(waitRooms);
-
-                    });
-
+            int[] answer = new int[places.length];
+            for (int i = 0; i < answer.length; i++) {
+                String[] place = places[i];
+                char[][] room = new char[place.length][];
+                for (int j = 0; j < room.length; j++) {
+                    room[j] = place[j].toCharArray();
+                }
+                if (isDistanced(room)) {
+                    answer[i] = 1;
+                } else {
+                    answer[i] = 0;
+                }
+            }
             return answer;
         }
 
-        /**
-         * 전체 대기실들을 상세 대기실 정보로 변경한다.
-         *
-         * @param place
-         * @return
-         */
-        char[][] placeToWaitRooms(String[] place) {
-            char[][] waitRooms = new char[5][5];
-            for (int i = 0; i < place.length; i++) {
-                char[] waitRoom = place[i].toCharArray();
-                for (int j = 0; j < waitRoom.length; j++) {
-                    waitRooms[i][j] = waitRoom[j];
-                }
-            }
-            return waitRooms;
-        }
 
-        /**
-         * 대기실 내용을 프린트한다.
-         *
-         * @param waitRooms
-         */
-        void printWaitRooms(char[][] waitRooms) {
-            Arrays.stream(waitRooms)
-                    .forEach(waitRoom -> {
-                        char[] charArr = waitRoom;
-                        System.out.printf("%c,%c,%c,%c,%c\n", charArr[0], charArr[1], charArr[2], charArr[3], charArr[4]);
-                    });
-            System.out.println();
-        }
-
-        /**
-         * 실제 맨해튼 거리를 잘지키는지확인한다.
-         * 모두 준수하는 경우는 1
-         * 한명이라도 준수하지 않는 경우 0을 반환한다.
-         * @param waitRooms 5*5 대기실 배치도 , 사람P , 책상O, 파티션X
-         * @return
-         */
-
-        int checkWaitRoom(char[][] waitRooms) {
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    char c = waitRooms[i][j];
-                    if(c == 'P'){
-                        boolean compliantResult = isCompliant(waitRooms, i, j);
-                        if(!compliantResult){
-                            return 0;
-                        }
+        // 거리두기 지킨지 판단하는 함수
+        private boolean isDistanced(char[][] room) {
+            for (int y = 0; y < room.length; y++) {
+                for (int x = 0; x < room[y].length; x++) {
+                    if (room[y][x] != 'P') { // 사람이 아닌경우 다음 값으로 넘어간다...
+                        continue;
+                    }
+                    if (!isDistanced(room, x, y)) {
+                        return false;
                     }
                 }
             }
-            return 1;
+            return true;
         }
 
-        /**
-         * 준수 여부를 판단한다. 준수한경우 true를 반환, 준수하지 않은 경우 false를 반환
-         * @param waitRooms
-         * @param pointX
-         * @param pointY
-         * @return
-         */
-        boolean isCompliant(char[][] waitRooms ,int pointY, int pointX){
-            //상
-            //하
-            //좌
-            //우
-            //우상
-            //우하
-            //좌상
-            //좌하
+        // 던져진 값이 사람인경우 거리두기 상세 체크
+        private boolean isDistanced(char[][] room, int x, int y) {
+            // 해당 인원의 상하좌우를 체크한다...
+            // 책상이 있는지 체크, 파티션은 괜찮다..
+            // 순서는 상좌 우하 순서
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                // 해당 상하 좌우가 규정된 범위를 벗어나는 경우 다시 방향을 바꿔서 돈다
+                if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) {
+                    continue;
+                }
+                // 해당값에 사람이 근접해있는가 있으면 거리두기 실패
+                // 책상인 경우 한번 더 근처에 사람이 있는 지 확인해야한다.
+                // 사람이 있는 경우 거리두기는 실패한다.
+                switch (room[ny][nx]) {
+                    case 'P':
+                        return false;
+                    case 'O':
+                        if (isNextToVolunteer(room, nx, ny, 3 - d)) {
+                            return false;
+                        }
+                    break;
+                }
+            }
+            return true;
+        }
+        // 3-d 값은
+        // dx[] = {0, -1, 1, 0}; // 상, 좌 , 우, 하
+        // dy[] = {-1, 0, 0, 1};
+        // 현재 방향의 반대방향은 조사할 필요가 없다 이미 처음에 했기때문에 배제히기 위한 값이다.
+        // 만약에 지금 방향이 우측이면 3-2 = 1, 1번 인덱스인 좌측 방향의 조사를 배제하고 (좌측은 이미 조사함)
+        // 만약에 지금 방향이 좌측이면 3-1 = 2, 2번 인덱스인 우측 방향의 조사를 배제한다 (우측은 이미 조사함)
+        private boolean isNextToVolunteer(char[][] room, int x, int y, int exclude) {
+            for (int d = 0; d < 4; d++) {
+                // 배제 방향은 조사하지 않는다.
+                if (d == exclude) {
+                    continue;
+                }
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                // 조사 범위를 벗어나면 조사하지 않는다.
+                if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) {
+                    continue;
+                }
+                // 사람이 있다면 거리두기 실패 이므로 true 반환
+                if (room[ny][nx] == 'P'){
+                    return true;
+                }
 
+            }
             return false;
+        }
+    }
+
+    /**
+     * 실제 정답
+     */
+    public static class Solution2 {
+        private static final int dx[] = {0, -1, 1, 0};
+        private static final int dy[] = {-1, 0, 0, 1};
+
+        private boolean isNextToVolunteer(char[][] room, int x, int y, int exclude) {
+            for (int d = 0; d < 4; d++) {
+                if (d == exclude) continue;
+
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length)
+                    continue;
+                if (room[ny][nx] == 'P') return true;
+            }
+            return false;
+        }
+
+        private boolean isDistanced(char[][] room, int x, int y) {
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length)
+                    continue;
+
+                switch (room[ny][nx]) {
+                    case 'P': return false;
+                    case 'O':
+                        if (isNextToVolunteer(room, nx, ny, 3- d)) return false;
+                        break;
+                }
+            }
+            return true;
+        }
+
+        private boolean isDistanced(char[][] room) {
+            for (int y = 0; y < room.length; y++) {
+                for (int x = 0; x < room[y].length; x++) {
+                    if (room[y][x] != 'P') continue;
+                    if (!isDistanced(room, x, y)) return false;
+                }
+            }
+            return true;
+        }
+
+        public int[] solution(String[][] places) {
+            int[] answer = new int[places.length];
+            for (int i = 0; i < answer.length; i++) {
+                String[] place = places[i];
+                char[][] room = new char[place.length][];
+                for (int j = 0; j < room.length; j++) {
+                    room[j] = place[j].toCharArray();
+                }
+                if (isDistanced(room)) {
+                    answer[i] = 1;
+                } else {
+                    answer[i] = 0;
+                }
+            }
+            return answer;
         }
     }
 }
